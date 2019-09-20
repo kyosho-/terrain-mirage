739591fa9aeffca0f6e2beb93f315a492b7501d1
kyosho- <info@kyosho.net>
2019-09-20 10:46:15 +0900

    add dynamic zip sample.

--- projects/terrain-mirage/src/app/app.module.ts
+++ projects/terrain-mirage/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +16,8 @@ import { PlotterComponent } from './content/plotter/plotter.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
